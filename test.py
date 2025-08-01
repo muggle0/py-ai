6d78cab454c0860c671d55f7205e5a58ddd029df
muggle <isocket@outlook.com>
2025-08-02 00:32:48 +0800

    py练习题

---
+++ test.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    # a=int(input())
+    # https: // github.com / python3xxx / Python - Every - Day?tab = readme - ov - file
+    # b=int(input())
+    # print("%d+%d=%d" % (a, b,a+b))
+    #
+    # c:int=1;
+    # print(id(c))
+    # c=1+2;
+    # print(id(c))
+    # c=c+1;
+    # print(id(c))
+    d:[]=[1,2]
+    print(id(d[1]))
+    d[1]=2
+    print(id(d[1]))
